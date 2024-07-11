1ce4101ba70591b9de71efe11992b3258b513339
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-07-11 17:33:28 +0330

    starting project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-slate-800'>hello new tecnos</h1>
+    </>
+  )
+}
+
+export default App
