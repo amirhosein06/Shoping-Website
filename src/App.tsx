f335d0e9e09e607346744650275ada91da6e3649
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-07-15 14:43:31 +0330

    added some libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import Navbar from "./components/navbar"
 import Slides from "./components/slides"
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import { useEffect } from "react"
+import axios from "axios"
 
 function App() {
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      try {
+        const response = await axios.get('https://api.escuelajs.co/api/v1/products');
+        console.log(response.data);
+        
+      } catch (error : any) {
+        console.error(error.message);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <Navbar />
       <Slides />
+      <Skeleton circle={false} height={50}/>
     </>
   )
 }
 
 export default App
-// api : https://fakeapi.platzi.com/en/rest/products/
+// api : https://api.escuelajs.co/api/v1/products
